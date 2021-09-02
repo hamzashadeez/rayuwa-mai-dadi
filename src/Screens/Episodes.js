df4136b35fedef9d0dec2bd640ef4312c6c69a9f
hamzashadeez <hahmad1178@gmail.com>
2021-09-01 22:43:25 -0700

    Added a loading components

--- src/Screens/Episodes.js
+++ src/Screens/Episodes.js
@@ -1,27 +1,44 @@
-import React, {useState, useEffect} from 'react'
-import Episode from '../Components/Episode';
-import {db} from '../Config/Firebase'
+import React, { useState, useEffect } from "react";
+import Episode from "../Components/Episode";
+import { db } from "../Config/Firebase";
+import './episode.css'
 
 function Episodes() {
-    const [episodes, setEpisodes] = useState([]);
+  const [episodes, setEpisodes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        db.collection("episodes").orderBy("timestamp", "desc").onSnapshot((shot) => {
-          setEpisodes(
-            shot.docs.map((doc) => ({
-              id: doc.id,
-              data: doc.data(),
-            }))
-          );
-        });
-      }, []);
-    return (
-        <div className='mx-3'>
-            <h5 className='mb-3 text-dark'>Episodes</h5>
-            {episodes.map(epi=> <Episode key={epi.id} data={epi.data} id={epi.id} />)}
-            
+  useEffect(() => {
+    db.collection("episodes")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((shot) => {
+        setEpisodes(
+          shot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        );
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <div className="mx-3">
+      <h5 className="mb-3 text-dark">Episodes</h5>
+      {loading ? (
+        <div className='loading'>
+          <div className='loading__box one'></div>
+          <div className='loading__box two'></div>
+          <div className='loading__box three'></div>
+          <div className='loading__box three'></div>
         </div>
-    )
+      ) : (
+        <div>
+          {episodes.map((epi) => (
+            <Episode key={epi.id} data={epi.data} id={epi.id} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Episodes
+export default Episodes;
